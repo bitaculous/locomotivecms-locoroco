4b9b7517a379fd5cb7ee479c23ad12f886bc87af
Maik Kempe <mkempe@bitaculous.com>
2014-08-18 11:08:18 +0200

    [➠] Minor formatting.

--- resources/config/deploy/sample.rb
+++ resources/config/deploy/sample.rb
@@ -1,4 +1,4 @@
-server '<server>', user: '<user>', roles: %w{web app db}
+server '<server>', user: '<user>', roles: %w[web app db]
 
 set :environment, '<environment>'
 set :rails_env, fetch(:environment)
@@ -9,6 +9,6 @@ set :linked_files, %w{config/application.yml config/environments/<stage>.rb conf
 
 set :ssh_options, {
   keys: '<identity_file>',
-  auth_methods: %w(publickey),
+  auth_methods: %w[publickey],
   forward_agent: false
 }
