4e77ea3a5f7334eb8964aa3f1544c4094670d23d
Maik Kempe <mkempe@bitaculous.com>
2014-10-27 15:25:43 +0100

    [➠] Modified routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bitaculous::Locoroco::Application.routes.draw do
   # Mount LocomotiveCMS as engine
   mount Locomotive::Engine => '/locomotive', as: 'locomotive'
+
+  match '/de/sitemap.xml' => 'locomotive/public/sitemaps#show', format: 'xml'
 end
