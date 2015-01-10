9e283803465d064851abec7b8ca1b0624977ef88
Maik Kempe <mkempe@bitaculous.com>
2015-01-10 17:42:59 +0100

    [➠] Updated comment (`application_controller`) [ci skip].

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  # A feature in Rails that protects against Cross-site Request Forgery (CSRF) attacks. This feature makes all generated
+  # forms have a hidden `id` field. This field must match the stored id or the form submission is not accepted. This
+  # prevents malicious forms on other sites or forms inserted with XSS from submitting to the Rails application.
   protect_from_forgery
 end
